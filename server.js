3d06103e288bbf9fd20fbf12f5ffc4e9c17abe5f
Ritik Singh <ritiksingh70045@gmail.com>
2025-05-14 20:41:09 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT;
+
+app.get('/',(req,res) => {
+  res.send('Welcome To the Home Page');
+})
+
+app.get('/about' , (req,res) => {
+  res.send('This is the About Page');
+})
+
+app.get('/login' , (req,res) => {
+  res.send('This is Login page');
+})
+
+app.listen(port , ()=>{
+  console.log(`Server running at port : ${port}`);
+})
